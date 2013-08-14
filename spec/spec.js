36a3c4c763c08d866e93b19a7e2606ec490af2e6
Cameron Boehmer <cameron.boehmer@gmail.com>
2013-08-13 17:26:59 -0700

    standardize organization for require config and test setup

--- spec/spec.js
+++ spec/spec.js
@@ -1,28 +1,12 @@
-require.config({
-  baseUrl: '.',
-  paths: {
-    'underscore': 'lib/underscore',
-    'mocha': 'lib/mocha',
-    'chai': 'lib/chai',
-    'variant': 'lib/variant',
-    'classPattern': 'lib/classPattern'
-  },
-  shim:{
-    'underscore':{
-      'exports': '_'
-    }
-  }
-});
-
-require(['mocha', 'chai', 'variant'], function(notMocha, chai, variant){
-  mocha.setup('bdd');
-
-  variant.setup(['stack.js','queue.js'], {
-    'pseudoclassical': 'Pseudoclassical',
-    'prototypal': 'Prototypal',
-    'functional-shared': 'Functional (shared)',
-    'functional': 'Functional'
-  }, 'functional');
-
-  window.expect = chai.expect;
+require(['variant'], function(variant){
+  variant.setup({
+    'files': ['stack.js','queue.js'],
+    'variants':{
+      'pseudoclassical': 'Pseudoclassical',
+      'prototypal': 'Prototypal',
+      'functional-shared': 'Functional (shared)',
+      'functional': 'Functional'
+    },
+    'default': 'functional'
+  });
 });
