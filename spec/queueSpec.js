85eee7c1ba156cf855de437a9e46ed37256d88c7
Cameron Boehmer <cameron.boehmer@gmail.com>
2013-08-15 15:26:12 -0700

    clean up variant interface, use nested requirejs statements in specs to load src

--- spec/queueSpec.js
+++ spec/queueSpec.js
@@ -1,66 +1,67 @@
-var deps = ['chai',
-            'mocha',
-            'variant',
-            'classPattern'];
+var deps = [
+  'chai',
+  'mocha',
+  'variant',
+  'classPattern'];
 define(deps, function(chai, notMocha, variant, classPattern){
+  require([variant('queue')], function(){
+    describe("queue", function() {
+      var queue;
+      var instantiator;
 
-  require(variant.pathFromFile('queue.js'));
+      if(variant.is('pseudoclassical')){
+        instantiator = Queue;
+        refreshQueue = function(){ queue = new Queue(); };
+      } else {
+        instantiator = makeQueue;
+        refreshQueue = function(){ queue = makeQueue(); };
+      }
 
-  describe("queue", function() {
-    var queue;
-    var instantiator;
+      beforeEach(refreshQueue);
 
-    if(variant.is('pseudoclassical')){
-      instantiator = Queue;
-      refreshQueue = function(){ queue = new Queue(); };
-    } else {
-      instantiator = makeQueue;
-      refreshQueue = function(){ queue = makeQueue(); };
-    }
+      it('should have "enqueue", "dequeue", and "size" methods', function() {
+        expect(queue.enqueue).to.be.a('function');
+        expect(queue.dequeue).to.be.a('function');
+        expect(queue.size).to.be.a('function');
+      });
 
-    beforeEach(refreshQueue);
+      it('should not error when dequeuing from an empty queue', function() {
+        expect(function(){queue.dequeue()}).not.throws();
+      });
 
-    it('should have "enqueue", "dequeue", and "size" methods', function() {
-      expect(queue.enqueue).to.be.a('function');
-      expect(queue.dequeue).to.be.a('function');
-      expect(queue.size).to.be.a('function');
+      it('should report its size correctly', function() {
+        var a = 'a', b = 'b', c = 'c';
+
+        queue.enqueue(a);
+        queue.enqueue(b);
+        queue.enqueue(c);
+        expect(queue.size()).equal(3);
+
+        queue.dequeue();
+        expect(queue.size()).equal(2);
+
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue(); // make sure we don't get to -1
+        expect(queue.size()).equal(0);
+      });
+
+      it('should dequeue items in the FIFO order', function() {
+        var a = 'a', b = 'b', c = 'c', d = 'd';
+
+        queue.enqueue(a);
+        queue.enqueue(b);
+        queue.enqueue(c);
+        expect(queue.dequeue()).equal(a);
+        expect(queue.dequeue()).equal(b);
+
+        queue.enqueue(d);
+        expect(queue.dequeue()).equal(c);
+        expect(queue.dequeue()).equal(d);
+      });
+
+      classPattern.ensure(instantiator).follows(variant());
     });
-
-    it('should not error when dequeuing from an empty queue', function() {
-      expect(function(){queue.dequeue()}).not.throws();
-    });
-
-    it('should report its size correctly', function() {
-      var a = 'a', b = 'b', c = 'c';
-
-      queue.enqueue(a);
-      queue.enqueue(b);
-      queue.enqueue(c);
-      expect(queue.size()).equal(3);
-
-      queue.dequeue();
-      expect(queue.size()).equal(2);
-
-      queue.dequeue();
-      queue.dequeue();
-      queue.dequeue(); // make sure we don't get to -1
-      expect(queue.size()).equal(0);
-    });
-
-    it('should dequeue items in the FIFO order', function() {
-      var a = 'a', b = 'b', c = 'c', d = 'd';
-
-      queue.enqueue(a);
-      queue.enqueue(b);
-      queue.enqueue(c);
-      expect(queue.dequeue()).equal(a);
-      expect(queue.dequeue()).equal(b);
-
-      queue.enqueue(d);
-      expect(queue.dequeue()).equal(c);
-      expect(queue.dequeue()).equal(d);
-    });
-
-    classPattern.ensure(instantiator).follows(variant.current);
-  });
+    variant.done();
+  })
 });
