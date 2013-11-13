50e318791e77ea7e07935d61e1ed8c07cc60d83c
Blake West <bwest87@gmail.com>
2013-11-12 21:39:15 -0800

    Cleaned of solutions

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,33 +3,16 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-/* START SOLUTION */
-  var start = -1;
-  var end = -1;
-/* END SOLUTION */
 
   // Implement the methods below
 
   instance.enqueue = function(value){
-/* START SOLUTION */
-    end++;
-    storage[end] = value;
-/* END SOLUTION */
   };
 
   instance.dequeue = function(){
-/* START SOLUTION */
-    instance.size() && start++;
-    var result = storage[start];
-    delete storage[start];
-    return result;
-/* END SOLUTION */
   };
 
   instance.size = function(){
-/* START SOLUTION */
-    return end - start;
-/* END SOLUTION */
   };
 
   return instance;
