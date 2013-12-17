2f4cb9e447a2fa811661e8f3e32b079a489d9aba
sekunovsky <sekunovsky@gmail.com>
2013-12-17 14:00:40 -0800

    moved data structures one solutions into solution branch

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,32 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional/stack.js and paste it here
+  /* START SOLUTION */
+  var instance = {};
+  _(instance).extend(stackMethods);
+
+  instance._storage = {};
+  instance._size = 0;
+
+  return instance;
+  /* END SOLUTION */
+};
+  
+var stackMethods = {};
+
+/* START SOLUTION */
+stackMethods.push = function(value){
+  this._storage[this._size] = value;
+  this._size++;
 };
 
-var stackMethods = {};
+stackMethods.pop = function(){
+  this._size && this._size--;
+  var result = this._storage[this._size];
+  delete this._storage[this._size];
+  return result;
+};
+
+stackMethods.size = function(){
+  return this._size;
+};
+/* END SOLUTION */
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,25 @@
 var Stack = function() {
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
+/* START SOLUTION */
+  this._storage = {};
+  this._size = 0;
+/* END SOLUTION */
 };
+
+/* START SOLUTION */
+Stack.prototype.push = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+Stack.prototype.pop = function(){
+  this._size && this._size--;
+  var result = this._storage[this._size];
+  delete this._storage[this._size];
+  return result;
+};
+
+Stack.prototype.size = function(){
+  return this._size;
+};
+/* END SOLUTION */
