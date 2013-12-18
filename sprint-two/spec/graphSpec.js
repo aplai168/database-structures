7c6605a48c37b21718c5ec06d4c582916a554f3b
sekunovsky <sekunovsky@gmail.com>
2013-12-18 11:28:56 -0800

    Converted sprint-two to use mocha instead of jasmine

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -1,3 +1,5 @@
+var assert = chai.assert;
+
 describe("graph", function() {
   var graph;
 
@@ -6,38 +8,38 @@ describe("graph", function() {
   });
 
   xit("should have methods named 'addNode', 'contains', 'removeNode', 'addEdge', 'getEdge', 'removeEdge' and 'forEachNode'", function() {
-    expect(graph.addNode).toEqual(jasmine.any(Function));
-    expect(graph.contains).toEqual(jasmine.any(Function));
-    expect(graph.removeNode).toEqual(jasmine.any(Function));
-    expect(graph.getEdge).toEqual(jasmine.any(Function));
-    expect(graph.addEdge).toEqual(jasmine.any(Function));
-    expect(graph.removeEdge).toEqual(jasmine.any(Function));
+    expect(graph.addNode).to.be.a('function');
+    expect(graph.contains).to.be.a('function');
+    expect(graph.removeNode).to.be.a('function');
+    expect(graph.getEdge).to.be.a('function');
+    expect(graph.addEdge).to.be.a('function');
+    expect(graph.removeEdge).to.be.a('function');
   });
 
   xit("should store values as nodes that were inserted", function() {
     graph.addNode("kittens");
     graph.contains("kittens");
-    expect(graph.contains("kittens")).toBe(true);
+    assert.isTrue(graph.contains("kittens"));
   });
 
   xit("should remove nodes that were inserted", function() {
     graph.addNode("puppies");
     graph.removeNode("puppies");
-    expect(graph.contains("puppies")).toBe(false);
+    assert.isFalse(graph.contains("puppies"));
   });
 
   xit("should automatically create an edge between two nodes if there is only one node in the graph", function() {
     graph.addNode("puppies");
     graph.addNode("kittens");
-    expect(graph.getEdge("puppies", "kittens")).toBe(true);
+    assert.isTrue(graph.getEdge("puppies", "kittens"));
   });
 
   xit("should create edges between two nodes", function() {
     graph.addNode("puppies");
     graph.addNode("kittens");
     graph.addNode("penguins", "puppies");
-    expect(graph.getEdge("penguins", "puppies")).toBe(true);
-    expect(graph.getEdge("penguins", "kittens")).toBe(false);
+    assert.isTrue(graph.getEdge("penguins", "puppies"));
+    assert.isFalse(graph.getEdge("penguins", "kittens"));
   });
 
   xit("should remove edges between nodes", function() {
@@ -46,15 +48,15 @@ describe("graph", function() {
     graph.addNode("satsumas", "bananas");
     graph.addEdge("satsumas", "apples")
     graph.removeEdge("apples", "bananas")
-    expect(graph.getEdge("apples", "bananas")).toBe(false);
+    assert.isFalse(graph.getEdge("apples", "bananas"));
   });
 
   xit("should remove nodes without any edges", function() {
     graph.addNode("jacket");
     graph.addNode("hat");
     graph.removeEdge("jacket", "hat");
-    expect(graph.contains("hat")).toBe(false);
-    expect(graph.contains("jacket")).toBe(false);
+    assert.isFalse(graph.contains("hat"));
+    assert.isFalse(graph.contains("jacket"));
   });
 
 });
--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -1,3 +1,6 @@
+var expect = chai.expect;
+var assert = chai.assert;
+
 describe("set", function() {
   var set;
 
@@ -6,21 +9,22 @@ describe("set", function() {
   });
 
   it("should have methods named 'add', 'contains', and 'remove'", function() {
-    expect(set.add).toEqual(jasmine.any(Function));
-    expect(set.contains).toEqual(jasmine.any(Function));
-    expect(set.remove).toEqual(jasmine.any(Function));
+    expect(set.add).to.be.a('function');
+    expect(set.contains).to.be.a('function');
+    expect(set.remove).to.be.a('function');
   });
 
   it("should add values to a set", function(){
     set.add('Susan Sarandon');
     set.add('Danny Glover');
-    expect(set.contains("Danny Glover")).toBe(true);
-    expect(set.contains("Susan Sarandon")).toBe(true);
+    assert.isTrue(set.contains("Danny Glover"));
+    assert.isTrue(set.contains("Susan Sarandon"));
   });
 
   it("should remove values from a set", function(){
     set.add('Mel Gibson');
     set.remove('Mel Gibson');
-    expect(set.contains('Mel Gibson')).toBe(false);
+    assert.isFalse(set.contains('Mel Gibson'));
   });
+
 });
