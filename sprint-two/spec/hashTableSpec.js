1bf6bf9a910a2e2b4b5c7e26b08de1a7404bee19
Josh Wyatt <joshwyatt@users.noreply.github.com>
2015-08-12 14:13:55 -0700

    Change extra credit to advanced

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -47,7 +47,7 @@ describe('hashTable', function() {
     window.getIndexBelowMaxForKey = oldHashFunction;
   });
 
-  // (Extra credit! Remove the extra "x" when you want the following tests to run)
+  // (Advanced! Remove the extra "x" when you want the following tests to run)
   xit('should double in size when needed', function() {
     _.each(people, function(person) {
       var firstName = person[0], lastName = person[1];
